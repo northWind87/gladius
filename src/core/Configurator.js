abaeb1700a492db02017885b52e1476ecd5ad903
Hasan Kamal-Al-Deen <hasank1987@gmail.com>
2011-10-26 17:35:07 -0400

    Line endings unified

--- src/core/Configurator.js
+++ src/core/Configurator.js
@@ -4,27 +4,27 @@
 
 
 define( function ( require ) {
-    
+
     /* Configuration Node
-     * 
+     *
      * Structure for storing configuration settings
      */
     var ConfNode = function( name, parent ) {
-        
+
         var self = this;
-        
+
         this.name = name;
         this.parent = parent;
         this.children = {};
         this.listeners = {};
-        
+
         // Node's configuration value
         var _value = '';
         Object.defineProperty( this, 'value', {
             get: function() {
                 return _value;
             },
-            
+
             set: function( value ) {
                 if ( _value !==  value ) {  // Additionally check if incoming value is string?
 
@@ -36,7 +36,7 @@ define( function ( require ) {
                 }
             }
         });
-        
+
         // Notifies us that a value stored somewhere in the subtree rooted by
         // this node has changed.
         this.notify = function ( path, newVal, oldVal ) {
@@ -61,26 +61,26 @@ define( function ( require ) {
                 this.parent.notify( path, newVal, oldVal );
             }
         };
-        
+
         // Traverse the node tree given a path
         this.traverse = function( path, doCreatePath ) {
-            
+
             var rv;
-            
+
             if ( path.length === 1 && path.charAt( 0 ) === '/' ) {
                 rv = this;
             } else {
-                
+
                 // Parse path and traverse the node tree
                 var pathElems = path.split('/'),
                     curNode = this,
                     successful = true;
-                    
+
                 for ( var i = 0, iMax = pathElems.length; successful && i < iMax; ++ i ) {
                     var curElem = pathElems[i];
-                    
+
                     if ( curElem !== '' ) {
-                        
+
                         // Look for name in children of current node
                         var nextNode = curNode.children[curElem];
                         if ( nextNode !== undefined ) {
@@ -96,16 +96,16 @@ define( function ( require ) {
                         }
                     }
                 }
-                
+
                 if ( successful ) {
                     rv = curNode;
                 }
             }
-            
+
             return rv;
         };
     };
-    
+
     /**
      * More or less unique id generator
      *
@@ -114,25 +114,25 @@ define( function ( require ) {
      */
     var _incCounter = 0;
     function uniqueId() {
-        
+
         var result, i, j;
         result = '';
-        
+
         for(j=0; j<32; j++)
         {
             if( j === 8 || j === 12|| j === 16|| j === 20) {
                 result = result + '-';
             }
-            
+
             i = Math.floor(Math.random()*16).toString(16).toUpperCase() + _incCounter;
             result = result + i;
-            
+
             ++ _incCounter;
         }
-        
+
         return result;
     }
-    
+
     /* Configurator
      *
      * Loads and stores configuration data. Allows external code to listen for
@@ -141,34 +141,34 @@ define( function ( require ) {
      * Initially written by Hasan (northWind) Kamal-Al-Deen
      */
     var Configurator = function( defaultConfiguration, node ) {
-        
+
         defaultConfiguration = defaultConfiguration || {};
-        
+
         var self = this;
-        
+
         this.node = node || new ConfNode( 'ROOT' );
-        
+
         this.id = uniqueId();
-        
+
         // Get a value based on a given path
         this.get = function( path ) {
             var rv = '',
                 targetNode = this.node.traverse( path );
-            
+
             if ( targetNode !== undefined ) {
                 rv = targetNode.value;
             }
-            
+
             return rv;
         };
-        
+
         // Set a value based on a given path
         this.set = function( path, value ) {
             var targetNode = this.node.traverse( path, true );
-            
+
             targetNode.value = value;
         };
-        
+
         // Update configuration with given json object
         this.update = function( json ) {
             for ( var key in json ) {
@@ -177,7 +177,7 @@ define( function ( require ) {
                 }
             }
         };
-        
+
         // Get a new configurator client for a node reachable using the given path.
         // If provided, associate listenerFunc with the newly created configurator client.
         // The listener func should accept upto 3 parameters:
@@ -188,14 +188,14 @@ define( function ( require ) {
         this.getPath = function( path, listenerFunc ) {
             var targetNode = this.node.traverse( path, true ),
                 rv = new Configurator( {}, targetNode );
-            
+
             if ( listenerFunc ) {
                 targetNode.listeners[rv.id] = listenerFunc;
             }
-            
+
             return rv;
         };
-        
+
         // Remove listener currently associated with client.
         this.ignore = function() {
             var curListener = this.node.listeners[this.id];
@@ -203,17 +203,17 @@ define( function ( require ) {
                 delete this.node.listeners[this.id];
             }
         };
-        
+
         // Set listener function for this client. If another listener is
         // associated with this client then that listener is first removed.
         this.listen = function( listenerFunc ) {
             if ( listenerFunc ) {
                 this.ignore();
-                
+
                 this.node.listeners[this.id] = listenerFunc;
             }
         };
-        
+
         // Load default configuration
         this.update( defaultConfiguration );
     };
